a5f4d3ab8a4cd134e147a4038076f0d6f28a9aa1
ilonaK <ikosko@alfabank.ru>
2023-01-22 13:28:43 +0300

    refactor: rewrite files to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { httpServer } from './http_server/index.js';
+import { mouse } from '@nut-tree/nut-js';
+
+import { startWS } from './ws-server/index.js';
+
+const HTTP_PORT = 8181;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
+
+startWS();
